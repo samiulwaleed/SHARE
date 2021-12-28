45984e21aebe6e656541acf7353d647732c08de6
Sami khan <samiulwaleed5556@gmail.com>
2021-12-29 00:30:14 +0500

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'http://127.0.0.1:3001', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
