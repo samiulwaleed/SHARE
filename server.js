f5614bb40acef6004a34918104e302bbc3f3c7c4
Sami khan <samiulwaleed5556@gmail.com>
2021-12-29 03:42:12 +0500

    make it better

--- server.js
+++ server.js
@@ -27,28 +27,8 @@ const corsOptions = {
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
   
 }
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(allowCrossDomain);
-  app.use(express.static(path.join(application_root, "public")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json());
 
 app.set('views', path.join(__dirname, '/views'));
