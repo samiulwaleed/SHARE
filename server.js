daca3040172a6d442e7eadd8ea7e98edb48a1fb9
Sami khan <samiulwaleed5556@gmail.com>
2021-12-29 04:01:56 +0500

    make it better

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const connectDB = require('./config/db');
 connectDB();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3001"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
