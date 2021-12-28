95b803530fc38ada852e96087b380a007ee8cd47
Sami khan <samiulwaleed5556@gmail.com>
2021-12-29 04:00:07 +0500

    make it better

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ app.use(express.static('public'));
 const connectDB = require('./config/db');
 connectDB();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // Cors 
 const corsOptions = {
   origin: 'http://127.0.0.1:3001'
