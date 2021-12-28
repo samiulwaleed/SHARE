a6fa2e8693e9cc201a945b18b203a27a26886b23
Sami khan <samiulwaleed5556@gmail.com>
2021-12-29 03:49:12 +0500

    make it better

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ connectDB();
 
 // Cors 
 const corsOptions = {
-  origin: 'http://127.0.0.1:3001'
+  origin: 'http://127.0.0.1:3000,http://127.0.0.1:3001,http://127.0.0.1:3002'
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
   
 }
