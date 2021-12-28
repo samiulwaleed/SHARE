0bdc88cf5b276df42d6699dcec6bdc83eb50bd33
Sami khan <samiulwaleed5556@gmail.com>
2021-12-29 01:41:20 +0500

    make it better

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const corsOptions = {
 //     "optionsSuccessStatus": 204
 //   }
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
